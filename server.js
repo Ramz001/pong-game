606ae9447992e0be3219983d385ffa55ba05dd25
Ramz001 <rkenjaev1@gmail.com>
2025-07-26 06:27:28 +0500

    Add express dependency and update server structure to separate API and socket handling

--- server.js
+++ server.js
@@ -1,58 +1,14 @@
-const server = require('http').createServer()
-const io = require('socket.io')(server)
-const path = require('path');
-const fs = require('fs');
-const publicDir = path.join(__dirname, "public");
+const http = require('http')
+const io = require('socket.io')
+
+const apiServer = require('./api')
+const sockets = require('./sockets')
+
+const httpServer = http.createServer(apiServer)
+const socketServer = io(httpServer)
 
 const PORT = 3000;
+httpServer.listen(PORT);
+console.log(`Listening on port ${PORT}...`);
 
-server.listen(PORT)
-
-console.log('listening port:', PORT)
-
-
-server.on('request', (req, res) => {
-  try {
-    // Let socket.io handle its own requests
-    if (req.url.startsWith('/socket.io/')) {
-      return;
-    }
-    const requestedPath = req.url === '/' ? 'index.html' : req.url;
-    const filePath = path.join(publicDir, requestedPath);
-    fs.createReadStream(filePath)
-      .on('error', () => {
-        fs.createReadStream(path.join(publicDir, 'index.html')).pipe(res);
-      })
-      .pipe(res);
-  } catch (error) {
-    console.error(error)
-  }
-});
-
-let readyPlayerCount = 0
-
-io.on("connection", (socket) => {
-  console.log('a user connected', socket.id)
-
-  socket.on('ready', () => {
-    console.log("Player ready", socket.id)
-    readyPlayerCount++;
-    console.log(readyPlayerCount)
-    if (readyPlayerCount % 2 === 0) {
-      io.emit('startGame', socket.id)
-    }
-  })
-
-  socket.on('paddleMove', (paddleData) => {
-    socket.broadcast.emit("paddleMove", paddleData)
-  })
-
-  socket.on('ballMove', (ballData) => {
-    socket.broadcast.emit("ballMove", ballData)
-  })
-
-  socket.on('disconnect',(reason) =>{
-    console.log(`Client ${socket.id} disconnected`)
-  })
-})
-
+sockets.listen(socketServer)
